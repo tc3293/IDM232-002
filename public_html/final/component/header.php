fc2ce7058ac4c2403a2610e74f81366e66b270d2
Tony Chen <91432463+tc3293@users.noreply.github.com>
2022-10-08 10:37:18 -0400

    update

--- public_html/final/component/header.php
+++ public_html/final/component/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public_html/final/dist/css/style.css">
-    <title>Document</title>
+    <title>IDM 232 final project</title>
 </head>
 <body>
 
