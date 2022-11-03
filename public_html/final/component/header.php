de4151f53b2ab5d2d62639ddbf7a0d4c16ec8237
Tony Chen <91432463+tc3293@users.noreply.github.com>
2022-11-03 00:25:47 -0400

    php

--- public_html/final/component/header.php
+++ public_html/final/component/header.php
@@ -4,8 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public_html/final/dist/css/style.css">
     <title>IDM 232 final project</title>
+    <link rel="stylesheet" href="../dist/css/style.css">
+    <link rel="shortcut icon" type="image/png" href="../dist/images/jicon.png">
+     <!--This is fontawesome.com script for font-->
+     <script src="https://kit.fontawesome.com/cba48a6fd7.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
+<!----------------Below here is MENU HEADER -------------------->
+<div id="header">
+    <div class="container">
+        <nav>
+            <a href="../index.php">
+            <img src= "../dist/images/J.png" class="logo">
+            </a>
+            <ul id= "sidemenu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Recipe</a></li>
+                <li><a href="#">Login In</a></li>
+                <i class="fas fa-times" onclick="closemenu()"></i>
+            </ul>
+            <i class="fas fa-bars" onclick="openmenu()"></i>
+        </nav>
+    </div>
+</div>
 
