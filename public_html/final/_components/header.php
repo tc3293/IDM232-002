1e3be5c376e2dc7c5080fd42a2853e7e9f56ad99
Tony Chen <91432463+tc3293@users.noreply.github.com>
2022-11-03 20:52:40 -0400

    update

--- public_html/final/_components/header.php
+++ public_html/final/_components/header.php
@@ -1,24 +1,39 @@
-<?php
 
-// if $page_title variable doesn't exist, create a default one
-if (!isset($page_title)) {
-    $page_title = '🚨 Missing Title 🚨';
-}
-$site_title = 'J-Watashi';
-$document_title = $page_title . ' | ' . $site_title; // Home | JAWN Clips
-
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/dist/images/favicon.ico">
-  <link rel="stylesheet" href="<?php echo site_url(); ?>/dist/styles/style.css">
-  <title><?php echo $document_title ; ?></title>
-</head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <title>IDM 232 final project</title>
+  <!--  <link rel="stylesheet" href="../dist/css/style.css"> 
+    <link rel="shortcut icon" type="image/png" href="/public_html/final/images/jicon.png">    --->
+    <link rel="stylesheet" href="dist/styles/style.css">
 
+
+
+
+
+     <!--This is fontawesome.com script for font-->
+     <script src="https://kit.fontawesome.com/cba48a6fd7.js" crossorigin="anonymous"></script>
+</head>
 <body>
-  <!-- Main Content Begins -->
+
+<!----------------Below here is MENU HEADER -------------------->
+<div id="header">
+    <div class="container">
+        <nav>
+            <a href="index.php">
+            <img src= "https://i.imgur.com/rMtn1MI.png" class="logo">
+            </a>
+            <ul id= "sidemenu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Recipe</a></li>
+                <li><a href="#">Login In</a></li>
+                <i class="fas fa-times" onclick="closemenu()"></i>
+            </ul>
+            <i class="fas fa-bars" onclick="openmenu()"></i>
+        </nav>
+    </div>
+</div>
