8ab52a1908dca6d38f44f8a7c6400ae0ce060e69
Tony Chen <91432463+tc3293@users.noreply.github.com>
2022-11-17 16:05:51 -0500

    update

--- public_html/final/_components/header.php
+++ public_html/final/_components/header.php
@@ -15,7 +15,7 @@ $document_title = $page_title . ' | ' . $site_title; // Home | JAWN Clips
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/dist/images/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/dist/images/jicon.png">
   <link rel="stylesheet" href="<?php echo site_url(); ?>/dist/styles/style.css">
   <title><?php echo $document_title ; ?></title>
 </head>
