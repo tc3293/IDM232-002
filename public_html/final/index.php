2dc3c32505a7ab9b4c807f59305855edc1de6481
Tony Chen <91432463+tc3293@users.noreply.github.com>
2022-11-17 20:34:01 -0500

    update

--- public_html/final/index.php
+++ public_html/final/index.php
@@ -11,7 +11,7 @@ include_once __DIR__ . '/_components/header.php';
         <div class="row">
             <div class="about-col-1">
                 <h1 class="sub-title">Dorayaki</h1>
-                <img src= "images/dorayaki.png" class="dora">
+                <img src= "dist/images/dorayaki.png" class="dora">
             </div>
             <div class="about-col-2">
             <h2>Ingredients</h2>
